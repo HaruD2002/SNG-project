2ae4d3eda2ce7e2e0892e7276a2ba3907c21418b
Haru Dinh <duynhatdinh2002@gmail.com>
2022-09-20 18:46:35 +0700

    final product

---
+++ src/java/controller/viewprofile/addFriend.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.viewprofile;
+
+import dao.FriendDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Date;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modal.User;
+
+/**
+ *
+ * @author duynh
+ */
+@WebServlet(name = "addFriend", urlPatterns = {"/addFriend"})
+public class addFriend extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("account");
+        Integer friendid = Integer.parseInt(request.getParameter("Friendcode"));
+        Date current = Date.valueOf(LocalDate.now());
+        try {
+            FriendDao db = new FriendDao();
+        db.pendingFriend(user.getID(), friendid, current);
+            System.out.println(friendid);
+            System.out.println(user.getDisplayname());
+            System.out.println(current);
+        } catch (Exception e) {
+        }
+          response.sendRedirect("viewProfile");
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/controller/viewprofile/updatePassword.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.viewprofile;
+
+import dao.UserDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modal.User;
+
+/**
+ *
+ * @author duynh
+ */
+@WebServlet(name = "updatePassword", urlPatterns = {"/updatePassword"})
+public class updatePassword extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        request.getRequestDispatcher("view/Profile/UpdatePassword.jsp").forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        HttpSession session = request.getSession();
+        User current = (User)session.getAttribute("account");
+        String currentPassword = request.getParameter("currentpass");
+        String newPassword = request.getParameter("newpassword");
+        UserDao db = new UserDao();
+        User pass = db.getPassword(current.getID());
+        
+        if(currentPassword.equals(pass.getPassword())){
+            db.changePassword(current.getID(), newPassword);
+            out.println("<script>");
+            out.println("alert('User password has been updated');");
+            out.println("location.replace('viewProfile');");
+            out.println("</script>");
+        }
+        else{
+            out.println("<script>");
+            out.println("alert('Your current password is not correct! pleaase try again');");
+            out.println("location.replace('updatePassword');");
+            out.println("</script>");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
